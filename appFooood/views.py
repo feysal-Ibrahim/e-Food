37dd53aeca4ac4f8f3556baa6269dde5cf46bbcb
feysal-Ibrahim <addictivefazman@gmail.com>
2018-11-30 04:25:22 +0300

    create coming soon page

---
+++ appFooood/views.py
@@ -0,0 +1,10 @@
+from django.views import generic
+from django.views.generic.edit import CreateView , UpdateView , DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render , redirect
+from django.views.generic import View
+from django.http import HttpResponse , Http404 , HttpResponseRedirect
+
+# Create your views here.
+def home(request):
+    return render( request , 'home.html' )
